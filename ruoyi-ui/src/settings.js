1ab7228c88b605b991bb176626e1c0912e823817
wufc <wu_fucheng8@163.com>
2026-08-02 16:01:16 +0800

    feat: rename stock analysis system

--- ruoyi-ui/src/settings.js
+++ ruoyi-ui/src/settings.js
@@ -62,5 +62,5 @@ module.exports = {
   /**
    * 底部版权文本内容
    */
-  footerContent: 'Copyright © 2018-2026 RuoYi. All Rights Reserved.'
+  footerContent: 'Copyright © 2026 520均线智析系统. All Rights Reserved.'
 }
